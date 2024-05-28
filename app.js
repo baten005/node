396bf0235a7f04c8b703c42846ec1a79a9d50748
baten005 <hrkhan16@cse.pstu.ac.bd>
2024-05-28 23:43:28 +0600

    p

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ app.get('/', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+app.get('/players', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT * FROM players where team_id=2');
+      res.json(result.rows);
+    } catch (error) {
+      console.error('Error fetching data from database:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
 app.listen(3030, () => {
   console.log('Server is running on port 3030');
